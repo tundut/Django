db77577848c3330961d6d684d70692da7c394f84
tundut <ducnguyenphan05@gmail.com>
2025-05-08 00:15:50 +0700

    add chuc nang

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
+    path('home/', views.home_view, name='home'),
+    path('manage/', views.manage_view, name='manage'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=reverse_lazy('home')), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
 import re
 
-# View đăng ký
 def register_view(request):
     if request.method == 'POST':
         email = request.POST.get('email')
@@ -36,21 +36,29 @@ def register_view(request):
 
     return render(request, 'register.html')
 
-# View đăng nhập
 def login_view(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
         
-        # Authenticate the user
         user = authenticate(request, username=username, password=password)
         
         if user is not None:
-            # If user is authenticated, log them in
             login(request, user)
-            return redirect('home')  # Redirect to a home page or dashboard
+            if user.is_staff:
+                return redirect('manage')
+            else:
+                return redirect('home')
         else:
             messages.error(request, "Invalid username or password.")
-            return redirect('login')  # Redirect back to login if authentication fails
+            return redirect('login')
     
     return render(request, 'login.html')
+
+def home_view(request):
+    return render(request, 'home.html')
+
+@staff_member_required
+def manage_view(request):
+    users = User.objects.all()
+    return render(request, 'manage.html', {'users': users})
